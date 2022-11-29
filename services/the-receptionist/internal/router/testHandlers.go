4ad929d7e6bd83b0b7372eb0d4d762e71b7b828b
Oskar Elvkull <oskar.elvkull@tibber.com>
2022-11-29 10:53:40 +0100

    Added things

--- services/the-receptionist/internal/router/testHandlers.go
+++ services/the-receptionist/internal/router/testHandlers.go
@@ -1,9 +1,32 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/Deathfireofdoom/big-corp-service-center/the-manager/protobuf/themanager"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
 
 func connectionTest(ctx *gin.Context) {
 	ctx.JSON(200, gin.H{
 		"message": "OK",
 	})
+}
+
+func theManageTest(ctx *gin.Context) {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewTheManagerClient(conn)
+
 }
